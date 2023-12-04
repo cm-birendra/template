d5ff66773182a5546be621211227976e11b1dfa1
Birendra Gurung <birendragurung007@gmail.com>
2023-12-04 10:41:49 +0545

    init

---
+++ vite-ts/src/pages/403.tsx
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet-async';
+// sections
+import { View403 } from 'src/sections/error';
+
+// ----------------------------------------------------------------------
+
+export default function Page403() {
+  return (
+    <>
+      <Helmet>
+        <title> 403 Forbidden</title>
+      </Helmet>
+
+    "neovim"
+      <View403 />
+    </>
+  );
+}
